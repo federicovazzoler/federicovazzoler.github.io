aeb03ae3d5d99d289a76cee2ba5895f2acd6e63d
Federico Vazzoler <vazzoler.federico@gmail.com>
2024-12-13 12:43:56 +0100

    Working dark/light mode toggle

--- federico_website/assets/script.js
+++ federico_website/assets/script.js
@@ -1,4 +1,11 @@
+// Description:
+// - On page load, it checks the localStorage for the dark mode preference and applies it accordingly by adding the dark class to the <body> element.
+// - It also sets the toggle checkbox state to checked if dark mode is enabled in localStorage.
+// - It listens for changes on the dark mode toggle and saves the preference in localStorage.
+
+// Add event listener to the website
 document.addEventListener('DOMContentLoaded', () => {
+  // Hamburgher menu button
   const hamburger = document.querySelector('.hamburger');
   const navLinks = document.querySelector('.nav-links');
 
@@ -6,7 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
     navLinks.classList.toggle('show');
   });
 
-  const darkModeToggle = document.getElementById('dark-mode-toggle');
+  // Dark mode button
+  const darkModeButton = document.querySelector('.toggle');
   const body = document.body;
 
   // Check localStorage for dark mode preference
@@ -15,8 +23,10 @@ document.addEventListener('DOMContentLoaded', () => {
     body.classList.add('dark');
   }
 
-  darkModeToggle.addEventListener('click', () => {
+  // Toggle dark mode when the button is clicked
+  darkModeButton.addEventListener('click', () => {
     body.classList.toggle('dark');
+    
     if (body.classList.contains('dark')) {
       localStorage.setItem('darkMode', 'enabled'); // Save preference
     } else {
